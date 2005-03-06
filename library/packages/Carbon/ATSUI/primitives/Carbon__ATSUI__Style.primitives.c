217976fe265783adb0519306a42333ca4d9aebe3
Steve Folta <steve@folta.net>
2005-03-06 01:46:09 +0000

    More work on the "Carbon ATSUI" package.
    git-archimport-id: steve@folta.net--cleet/trylon--main--1.0--patch-29

--- library/packages/Carbon/ATSUI/primitives/Carbon__ATSUI__Style.primitives.c
+++ library/packages/Carbon/ATSUI/primitives/Carbon__ATSUI__Style.primitives.c
@@ -45,6 +45,127 @@ obj_ size_co___Carbon__ATSUI__Style(obj_ this_, obj_ new_size)
 	size = Long2Fix(IntValue_(new_size));
 	ATSUSetAttributes((ATSUStyle) this_->fields[0], 1, &tag, &valueSize,
 	                  &valuePtr);
+	return NULL;
+}
+
+
+obj_ is_bold__Carbon__ATSUI__Style(obj_ this_)
+{
+	Boolean value;
+	ByteCount actualParamSize;
+
+	ATSUGetAttribute((ATSUStyle) this_->fields[0], kATSUQDBoldfaceTag,
+	                 sizeof(Boolean), &value, &actualParamSize);
+	return (value ? true__Standard : false__Standard);
+}
+
+
+obj_ is_bold_co___Carbon__ATSUI__Style(obj_ this_, obj_ new_value)
+{
+	ATSUAttributeTag tag = kATSUQDBoldfaceTag;
+	Boolean value = (new_value == true__Standard);
+	ByteCount valueSize = sizeof(Boolean);
+	ATSUAttributeValuePtr valuePtr = &value;
+
+	ATSUSetAttributes((ATSUStyle) this_->fields[0], 1, &tag, &valueSize,
+	                  &valuePtr);
+	return NULL;
+}
+
+
+obj_ is_italic__Carbon__ATSUI__Style(obj_ this_)
+{
+	Boolean value;
+	ByteCount actualParamSize;
+
+	ATSUGetAttribute((ATSUStyle) this_->fields[0], kATSUQDItalicTag,
+	                 sizeof(Boolean), &value, &actualParamSize);
+	return (value ? true__Standard : false__Standard);
+}
+
+
+obj_ is_italic_co___Carbon__ATSUI__Style(obj_ this_, obj_ new_value)
+{
+	ATSUAttributeTag tag = kATSUQDItalicTag;
+	Boolean value = (new_value == true__Standard);
+	ByteCount valueSize = sizeof(Boolean);
+	ATSUAttributeValuePtr valuePtr = &value;
+
+	ATSUSetAttributes((ATSUStyle) this_->fields[0], 1, &tag, &valueSize,
+	                  &valuePtr);
+	return NULL;
+}
+
+
+obj_ is_underlined__Carbon__ATSUI__Style(obj_ this_)
+{
+	Boolean value;
+	ByteCount actualParamSize;
+
+	ATSUGetAttribute((ATSUStyle) this_->fields[0], kATSUQDUnderlineTag,
+	                 sizeof(Boolean), &value, &actualParamSize);
+	return (value ? true__Standard : false__Standard);
+}
+
+
+obj_ is_underlined_co___Carbon__ATSUI__Style(obj_ this_, obj_ new_value)
+{
+	ATSUAttributeTag tag = kATSUQDUnderlineTag;
+	Boolean value = (new_value == true__Standard);
+	ByteCount valueSize = sizeof(Boolean);
+	ATSUAttributeValuePtr valuePtr = &value;
+
+	ATSUSetAttributes((ATSUStyle) this_->fields[0], 1, &tag, &valueSize,
+	                  &valuePtr);
+	return NULL;
+}
+
+
+obj_ is_condensed__Carbon__ATSUI__Style(obj_ this_)
+{
+	Boolean value;
+	ByteCount actualParamSize;
+
+	ATSUGetAttribute((ATSUStyle) this_->fields[0], kATSUQDCondensedTag,
+	                 sizeof(Boolean), &value, &actualParamSize);
+	return (value ? true__Standard : false__Standard);
+}
+
+
+obj_ is_condensed_co___Carbon__ATSUI__Style(obj_ this_, obj_ new_value)
+{
+	ATSUAttributeTag tag = kATSUQDCondensedTag;
+	Boolean value = (new_value == true__Standard);
+	ByteCount valueSize = sizeof(Boolean);
+	ATSUAttributeValuePtr valuePtr = &value;
+
+	ATSUSetAttributes((ATSUStyle) this_->fields[0], 1, &tag, &valueSize,
+	                  &valuePtr);
+	return NULL;
+}
+
+
+obj_ is_extended__Carbon__ATSUI__Style(obj_ this_)
+{
+	Boolean value;
+	ByteCount actualParamSize;
+
+	ATSUGetAttribute((ATSUStyle) this_->fields[0], kATSUQDExtendedTag,
+	                 sizeof(Boolean), &value, &actualParamSize);
+	return (value ? true__Standard : false__Standard);
+}
+
+
+obj_ is_extended_co___Carbon__ATSUI__Style(obj_ this_, obj_ new_value)
+{
+	ATSUAttributeTag tag = kATSUQDExtendedTag;
+	Boolean value = (new_value == true__Standard);
+	ByteCount valueSize = sizeof(Boolean);
+	ATSUAttributeValuePtr valuePtr = &value;
+
+	ATSUSetAttributes((ATSUStyle) this_->fields[0], 1, &tag, &valueSize,
+	                  &valuePtr);
+	return NULL;
 }
 
 
