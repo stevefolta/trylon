9977bc141665c2ffed403d2f5173151fceb75a84
Steve Folta <steve@folta.net>
2005-03-27 23:31:27 +0000

    Changed Standard__Char.primitives.c, so the functions no longer try to handle Symbols/Strings as Chars, now that we have real Char literals.
    git-archimport-id: steve@folta.net--cleet/trylon--main--1.0--patch-63

--- library/packages/Standard/primitives/Standard__Char.primitives.c
+++ library/packages/Standard/primitives/Standard__Char.primitives.c
@@ -5,68 +5,57 @@
 #define value_(charObj) 	(((struct Standard__Int__internal*) charObj)->value)
 
 
-static obj_ Charify(obj_ arg)
-{
-	UsingMethod_(is_a_co_)
-	UsingMethod_(first_character)
-
-	if (_Test_(Call_(is_a_co_, arg, (obj_) &Standard__String)))
-		arg = Call_(first_character, arg);
-	return arg;
-}
-
-
 obj_ _eq__eq___Standard__Char(obj_ this_, obj_ arg)
 {
 	return
-		(value_(this_) == value_(Charify(arg)) ? true__Standard : false__Standard);
+		(value_(this_) == value_(arg) ? true__Standard : false__Standard);
 }
 
 
 obj_ _nt__eq___Standard__Char(obj_ this_, obj_ arg)
 {
 	return
-		(value_(this_) != value_(Charify(arg)) ? true__Standard : false__Standard);
+		(value_(this_) != value_(arg) ? true__Standard : false__Standard);
 }
 
 
 obj_ _lt___Standard__Char(obj_ this_, obj_ arg)
 {
 	return
-		(value_(this_) < value_(Charify(arg)) ? true__Standard : false__Standard);
+		(value_(this_) < value_(arg) ? true__Standard : false__Standard);
 }
 
 
 obj_ _lt__eq___Standard__Char(obj_ this_, obj_ arg)
 {
 	return
-		(value_(this_) <= value_(Charify(arg)) ? true__Standard : false__Standard);
+		(value_(this_) <= value_(arg) ? true__Standard : false__Standard);
 }
 
 
 obj_ _gt___Standard__Char(obj_ this_, obj_ arg)
 {
 	return
-		(value_(this_) > value_(Charify(arg)) ? true__Standard : false__Standard);
+		(value_(this_) > value_(arg) ? true__Standard : false__Standard);
 }
 
 
 obj_ _gt__eq___Standard__Char(obj_ this_, obj_ arg)
 {
 	return
-		(value_(this_) >= value_(Charify(arg)) ? true__Standard : false__Standard);
+		(value_(this_) >= value_(arg) ? true__Standard : false__Standard);
 }
 
 
 obj_ _pl___Standard__Char(obj_ this_, obj_ arg)
 {
-	return BuildChar_(value_(this_) + value_(Charify(arg)));
+	return BuildChar_(value_(this_) + value_(arg));
 }
 
 
 obj_ ___Standard__Char(obj_ this_, obj_ arg)
 {
-	return BuildChar_(value_(this_) + value_(Charify(arg)));
+	return BuildChar_(value_(this_) + value_(arg));
 }
 
 
