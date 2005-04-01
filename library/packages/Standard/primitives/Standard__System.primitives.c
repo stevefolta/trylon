c57344577aa46d5e23f54f1225f7ee78f8cc3888
Steve Folta <steve@folta.net>
2005-04-01 09:13:12 +0000

    Added Standard System.
    git-archimport-id: steve@folta.net--cleet/trylon--main--1.0--patch-74

---
+++ library/packages/Standard/primitives/Standard__System.primitives.c
@@ -0,0 +1,29 @@
+/* Standard__System.primitives.c */
+
+#include "Trylon_.h"
+#ifdef OSX_FINK
+	// Fink doesn't use the gc directory for some reason.
+	#include <gc.h>
+#else
+	#include <gc/gc.h>
+#endif
+
+
+obj_ milliseconds__Standard__System()
+{
+	struct timeval timeVal;
+	struct timezone timeZone;
+	long long now;
+
+	gettimeofday(&timeVal, &timeZone);
+	now = ((long long) timeVal.tv_sec) * 1000000LL + timeVal.tv_usec;
+	return BuildInt_(now / 1000);
+}
+
+
+obj_ num_garbage_collections__Standard__System()
+{
+	return BuildInt_(GC_gc_no);
+}
+
+
