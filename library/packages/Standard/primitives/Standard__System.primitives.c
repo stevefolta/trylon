0d3b20104cf16fc74e29eb73d90f3ae8951910df
Steve Folta <steve@folta.net>
2005-07-28 22:32:27 +0000

    Fixed ForContext to allow setting the variable.  Added "System sleep-ms:".
    git-archimport-id: steve@folta.net--cleet/trylon--main--1.0--patch-194

--- library/packages/Standard/primitives/Standard__System.primitives.c
+++ library/packages/Standard/primitives/Standard__System.primitives.c
@@ -2,6 +2,7 @@
 
 #include "Trylon_.h"
 #include <sys/time.h>
+#include <time.h>
 #include <signal.h>
 #ifdef OSX_FINK
 	// Fink doesn't use the gc directory for some reason.
@@ -20,6 +21,16 @@ obj_ milliseconds__Standard__System()
 	gettimeofday(&timeVal, &timeZone);
 	now = ((long long) timeVal.tv_sec) * 1000000LL + timeVal.tv_usec;
 	return BuildInt_(now / 1000);
+}
+
+
+obj_ sleep_ms_co___Standard__System(obj_ milliseconds)
+{
+	int ms = IntValue_(milliseconds);
+	struct timespec timeSpec;
+	timeSpec.tv_sec = ms / 1000;
+	timeSpec.tv_nsec = (ms % 1000) * 1000 * 1000;
+	nanosleep(&timeSpec, NULL);
 }
 
 
