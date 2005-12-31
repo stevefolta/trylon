ba0ad646e600fd6c86e775445f16be145abafc48
Steve Folta <steve@folta.net>
2005-12-31 03:08:11 +0000

    Various fixes, mostly to TrylonCompiler CLexer.
    git-archimport-id: steve@folta.net--cleet/trylon--main--1.0--patch-214

--- library/packages/TrylonCompiler/primitives/TrylonCompiler__CLexer.primitives.c
+++ library/packages/TrylonCompiler/primitives/TrylonCompiler__CLexer.primitives.c
@@ -38,6 +38,7 @@ static obj_ token(obj_ symbol)
 {
   extern obj_ allocate_object_co___Standard__Implementation(obj_);
   extern obj_ create_co___TrylonCompiler__Token(obj_, obj_);
+	extern class_spec_ TrylonCompiler__Token;
 
 	obj_ token =
 		allocate_object_co___Standard__Implementation(
@@ -47,15 +48,16 @@ static obj_ token(obj_ symbol)
 }
 
 
-static void textToken(obj_ symbol, char* start, char* stopper)
+static obj_ textToken(obj_ symbol, char* start, char* stopper)
 {
 	obj_ token;
   extern obj_ allocate_object_co___Standard__Implementation(obj_);
   extern obj_ create_co_text_co___TrylonCompiler__Token(obj_, obj_, obj_);
+	extern class_spec_ TrylonCompiler__Token;
 
-	// Build the string object.
-	struct Standard__String__Internal* strObj =
-		(struct Standard__String__internal*)
+	/* Build the string object. */
+	struct Standard__String__internal* strObj = 
+		(struct Standard__String__internal*) 
 			Allocate_(sizeof(struct Standard__String__internal));
 	strObj->class_ = StdClassRef_(String);
 #ifndef SEMI_PRIMITIVE_STRINGS_
@@ -89,6 +91,8 @@ static obj_ possiblyEqualsToken(CLexer* self, obj_ without, obj_ with)
 static void throwError(obj_ message)
 {
 	extern obj_ create_co___Standard__MessageException(obj_, obj_);
+	extern obj_ allocate_object_co___Standard__Implementation(obj_);
+	extern class_spec_ Standard__MessageException;
 
 	obj_ exception = 
 		allocate_object_co___Standard__Implementation(
@@ -98,7 +102,7 @@ static void throwError(obj_ message)
 }
 
 
-obj_ create_co___TrylonCompiler__Lexer(obj_ this_, obj_ text)
+obj_ create_co___TrylonCompiler__CLexer(obj_ this_, obj_ text)
 {
 	CLexer* self = (CLexer*) this_;
 	UsingMethod_(start)  UsingMethod_(stopper)
@@ -108,30 +112,32 @@ obj_ create_co___TrylonCompiler__Lexer(obj_ this_, obj_ text)
 
 	self->p = BytePtrValue_(Call_(start, text));
 	self->stopper = BytePtrValue_(Call_(stopper, text));
-	self->atLineStart = true;
-	self->unindentsPending = false;
+	self->atLineStart = 1;
+	self->unindentsPending = 0;
 	self->stackTop = Allocate_(sizeof(IntStackCell_));
 	self->stackTop->value = 0;
 	self->stackTop->next = NULL;
 }
 
 
-obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
+obj_ next_token__TrylonCompiler__CLexer(obj_ this_)
 { 
+	CLexer* self = (CLexer*) this_;
 	int indentation;
 	char c, nextChar;
-	char* tokenStart, tokenEnd;
+	char* tokenStart;
+	char* tokenEnd;
 	obj_ type;
 	bool isIDChar, isHex;
 	UsingSym_(indent) UsingSym_(unindent)
 	UsingSym_(name) UsingSym_(keyword)
 	UsingSym_(eol) UsingSym_(eof)
-	UsingSym_(_) UsingSym(__eq_)
+	UsingSym_(_) UsingSym_(__eq_)
 	UsingSym_(int_literal) UsingSym_(float_literal) UsingSym_(character_literal)
-	UsingSym_(string_literal) UsingSym(symbol_literal)
+	UsingSym_(string_literal) UsingSym_(symbol_literal)
 	UsingSym_(comment)
 	UsingSym_(_40_) UsingSym_(_41_) UsingSym_(_123_) UsingSym_(_125_)
-	UsingSym_(_91_) UsingSym_(_93_) UsingSym_(_46_) UsingSym_(_44_)
+	UsingSym_(_in_) UsingSym_(_dx_) UsingSym_(_46_) UsingSym_(_44_)
 	UsingSym_(_59_) UsingSym_(_tw_)
 	UsingSym_(_pl_) UsingSym_(_pl__eq_)
 	UsingSym_(_eq_) UsingSym_(_eq__eq_)
@@ -141,8 +147,10 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 	UsingSym_(_xr_) UsingSym_(_xr__eq_)
 	UsingSym_(_nt_) UsingSym_(_nt__eq_)
 	UsingSym_(_co_) UsingSym_(_co__eq_)
-	UsingSym_(_lt_) UsingSym_(_lt__eq_) UsingSym_(_lt__lt_) UsingSym(_lt__lt__eq_)
-	UsingSym_(_gt_) UsingSym_(_gt__eq_) UsingSym_(_gt__gt_) UsingSym(_gt__gt__eq_)
+	UsingSym_(_lt_) UsingSym_(_lt__eq_)
+	UsingSym_(_lt__lt_) UsingSym_(_lt__lt__eq_)
+	UsingSym_(_gt_) UsingSym_(_gt__eq_)
+	UsingSym_(_gt__gt_) UsingSym_(_gt__gt__eq_)
 	UsingSym_(_or_) UsingSym_(_or__eq_) UsingSym_(_or__or_)
 	UsingSym_(_an_) UsingSym_(_an__eq_) UsingSym_(_an__an_)
 	DefineString_(0, "Indentation error!", 18)
@@ -153,11 +161,11 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 	DefineString_(5, "\"", 1)
 
 	if (self->unindentsPending) {
-		if (stackTop == NULL)
+		if (self->stackTop == NULL)
 			throwError(Str_(0));
-		if (unindentTo == self->stackTop->value) {
+		if (self->unindentTo == self->stackTop->value) {
 			/* We're done unindenting. */
-			self->unindentsPending = false;
+			self->unindentsPending = 0;
 			}
 		else {
 			/* Unindent one more level. */
@@ -169,7 +177,7 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 	while (1) {
 		/* If at line start, figure out the indentation. */
 		if (self->atLineStart) {
-			self->atLineStart = false;
+			self->atLineStart = 0;
 
 			/* Get the indentation */
 			indentation = 0;
@@ -185,17 +193,17 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 			/* If it's just an empty line, emit an EOL. */
 			if (c == '\n' || c == '\r') {
 				self->p += 1;
-				self->atLineStart = true;
+				self->atLineStart = 1;
 				return token(Sym_(eol));
 				}
 
 			/* Figure out the indentation change. */
 			if (indentation > self->stackTop->value) {
-				push(indentation);
+				push(self, indentation);
 				return token(Sym_(indent));
 				}
 			else if (indentation < self->stackTop->value) {
-				self->unindentsPending = false;
+				self->unindentsPending = 0;
 				self->unindentTo = indentation;
 				self->stackTop = self->stackTop->next;
 				return token(Sym_(unindent));
@@ -212,10 +220,18 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 				return token(Sym_(eof));
 				break;
 
+			/* Line break. */
 			case '\n':
 			case '\r':
-				self->atLineStart = true;
+				self->atLineStart = 1;
 				return token(Sym_(eol));
+				break;
+
+			/* Whitespace. */
+			case ' ':
+			case '\t':
+				/* Skip whitespace. */
+				tokenStart = self->p;
 				break;
 
 			case '-':
@@ -247,14 +263,14 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 
 				else if (nextChar >= '0' && nextChar <= '9') {
 					/* Number. */
-					bool isFloat = false;
+					bool isFloat = 0;
 					self->p += 1;
 					while (1) {
 						c = curChar(self);
 						if (c >= '0' && c <= '9')
 							self->p += 1;
 						else if (c == '.') {
-							isFloat = true;
+							isFloat = 1;
 							self->p += 1;
 							}
 						else {
@@ -265,7 +281,7 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 					}
 
 				else
-					return token(_);
+					return token(Sym_(_));
 				break;
 			
 			case '#':
@@ -319,7 +335,7 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 					else if (c == '\'')
 						break;
 					}
-				return token(Sym_(symbol_literal), tokenStart, tokenEnd);
+				return textToken(Sym_(symbol_literal), tokenStart, tokenEnd);
 				break;
 
 			case '`':
@@ -349,9 +365,9 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 			case '}':
 				return token(Sym_(_125_));
 			case '[':
-				return token(Sym_(_91_));
+				return token(Sym_(_in_));
 			case ']':
-				return token(Sym_(_93_));
+				return token(Sym_(_dx_));
 			case '.':
 				return token(Sym_(_46_));
 			case ',':
@@ -365,7 +381,7 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 			case '+':
 				return possiblyEqualsToken(self, Sym_(_pl_), Sym_(_pl__eq_));
 			case '=':
-				return possiblyEqualsToken(self, Sym(_eq_), Sym_(_eq__eq_));
+				return possiblyEqualsToken(self, Sym_(_eq_), Sym_(_eq__eq_));
 			case '*':
 				return possiblyEqualsToken(self, Sym_(_st_), Sym_(_st__eq_));
 			case '/':
@@ -426,7 +442,7 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 				nextChar = curChar(self);
 				if (nextChar == '&') {
 					self->p += 1;
-					type = Sym_(_an_an_);
+					type = Sym_(_an__an_);
 					}
 				else if (nextChar == '=') {
 					self->p += 1;
@@ -474,12 +490,12 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 			/* Number. */
 			case '0' ... '9':
 				/* Is it hex? */
-				isHex = false;
+				isHex = 0;
 				if (c == '0') {
 					c = curChar(self);
 					if (c == 'x' || c == 'X') {
 						self->p += 1;
-						isHex = true;
+						isHex = 1;
 						}
 					}
 				/* Read the rest of the number. */
@@ -503,8 +519,8 @@ obj_ next_token__TrylonCompiler__Lexer(obj_ this_)
 			/* Unknown character. */
 			default:
 				{
-				UsingMethod_(_pl_) UsingMethod_(string_)
-				obj_ message = Call_(string_, BuildChar_(c));
+				UsingMethod_(_pl_) UsingMethod_(string)
+				obj_ message = Call_(string, BuildChar_(c));
 				message = Call_(_pl_, Str_(4), message);
 				message = Call_(_pl_, message, Str_(5));
 				throwError(message);
